0f9a80d592fbd75cc681bfb26ad54a1a7824b614
Najamuddin786 <786mdnajamuddin@gmail.com>
2024-09-06 12:01:13 +0530

    V1

---
+++ db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const connection = mongoose.connect('mongodb://localhost:27017/pay')
+
+
+export default connection;
---
+++ index.js
@@ -0,0 +1,30 @@
+import express, { json } from "express";
+import connection from "./db.js"; // Assuming your MongoDB connection is handled in this file
+import cors from cors
+
+
+let app = express();
+let port = 3000;
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(json());
+
+// Define routes
+app.get('/home', (req, res) => {
+    res.send("This is the home page"); // Corrected the response method
+});
+app.get('/product', (req, res) => {
+    res.send("This is the product page"); // Corrected the response method
+});
+
+// Start server after database connection
+app.listen(port,async()=>{
+    try {
+        await connection
+        console.log("Mongodb connected")
+    } catch (error) {
+        console.log("Mongodb not connected")
+    }
+    console.log(`Server is running port number ${port}`)
+});
