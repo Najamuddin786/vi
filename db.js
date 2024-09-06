63bfd420916caee51f6746817581f84a91451d24
Najamuddin786 <786mdnajamuddin@gmail.com>
2024-09-06 12:44:21 +0530

    new

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+let m='mongodb+srv://786mdnajamuddin:okB7btwDZxARl7q5@cluster0.d0b4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// okB7btwDZxARl7q5  786mdnajamuddin
 
-const connection = mongoose.connect('mongodb://localhost:27017/pay')
+const connection = mongoose.connect(m)
+
 
 
 export default connection;
